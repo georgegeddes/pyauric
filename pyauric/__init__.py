fe4e42dde8c1f2fd437b40fcfc0cff8d4a941e46
George Geddes <georgegdds@gmail.com>
2016-02-18 14:19:02 -0500

    fixed parm parser and better default params

--- pyauric/__init__.py
+++ pyauric/__init__.py
@@ -272,7 +272,7 @@ def parse_params( filename ):
     parsed_lines=[]
     for i, line in enumerate(lines):
         if re.search(":$",line): 
-            parsed_lines.append([line]) 
+            parsed_lines.append(line) 
             continue
         x = re.search('.*(?==)',line)                         # Everything before =
         y = re.search('(?<==)*[0-9 -]*?\.*[0-9 ]*(?=:)',line) # Number between = and :
@@ -313,7 +313,7 @@ _param_format = r"""Mandatory parameters:
        YYDDD =      92080 : year & day (YYDDD format)
        UTSEC =   45000.00 : universal time (sec)
         GLAT =      42.00 : latitude (deg)
-        GLON =     289.00 : longitude (deg)
+        GLON =     000.00 : longitude (deg)
    SCALE(N2) =       1.00 : N2 density scale factor
    SCALE(O2) =       1.00 : O2 density scale factor
     SCALE(O) =       1.00 : O  density scale factor
@@ -327,17 +327,16 @@ Derived parameters:
        GMLAT =      51.84 : geomagnetic latitude (deg)
        GMLON =       1.71 : geomagnetic longitude (deg)
        DPANG =      70.16 : magnetic dip angle (deg)
-         SZA =       0.00 : solar zenith angle (deg)
+         SZA =      30.00 : solar zenith angle (deg)
          SLT =       1.00 : solar local time (hours)
       F10DAY =      79.30 : F10.7 (current day)
       F10PRE =      76.80 : F10.7 (previous day)
       F10AVE =      79.40 : F10.7 (81-day average)
        AP(1) =       9.00 : daily Ap
-       AP(2) =      -1.00 : 3-hour Ap #
+       AP(2) =      -1.00 : 3-hour Ap
        AP(3) =      -1.00 : 3-hour Ap
        AP(4) =      -1.00 : 3-hour Ap
        AP(5) =      -1.00 : 3-hour Ap
        AP(6) =      -1.00 : average 3-hour Ap
-       AP(7) =      -1.00 : average 3-hour Ap
-"""
+       AP(7) =      -1.00 : average 3-hour Ap"""
 

a8cd1cce88a69762ebbfc9aabfaa7b4f2c60973c
George Geddes <georgegdds@gmail.com>
2019-08-15 18:35:02 -0400

    move matplotlib to extras

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 setup(name = 'pyauric',
       packages = ['pyauric'], # this must be the same as the name above
-      version = '0.2',
+      version = '0.2.1',
       description = 'A python interface for the AURIC model.',
       author = 'George Geddes',
       author_email = 'george_geddes@student.uml.edu',
       keywords = ['AURIC','auric','radiative transfer','radiative transport'], # arbitrary keywords
-      install_requires = ['numpy', 'fortranformat', 'pandas', 'matplotlib'],
+      install_requires = ['numpy', 'fortranformat', 'pandas'],
+      extras_require={"plotting": "matplotlib"},
       test_suite = "tests.test_all",
 )
